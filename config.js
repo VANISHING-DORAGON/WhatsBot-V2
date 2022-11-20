d790d68e1096cc6bb08165432c8dff74cf8112d6
VANISHING-DORAGON <118322339+VANISHING-DORAGON@users.noreply.github.com>
2022-11-20 03:12:52 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,32 +1,32 @@
 module.exports = {
   profile: {
-    ownerName: "", // defult: AiDarkEzio
-    ownerNumb: "", // defult: 94761539856
-    botName: "", // defult: Whats Bot MD
+    ownerName: "HYOUDOU ISSEI-REDDO DORAGON", // defult: AiDarkEzio
+    ownerNumb: "22892392619", // defult: 94761539856
+    botName: "VANISHING DORAGON", // defult: Whats Bot MD
   },
   reply: {
-    notFound: "", // defult: "This Command not created. it was creating",
-    success: "", // defult: "Done ✓",
-    admin: "", // defult: "This Feature Is Only For Admin!",
-    botAdmin: "", // defult: "Bot Must Be Admin First!",
-    owner: "", // defult: "This Feature Is Only For Owner!",
-    group: "", // defult: "Feature Used Only For Groups!",
-    private: "", // defult: "Features Used Only For Private Chat!",
-    bot: "", // defult: "This Feature Is Only For Bot",
-    wait: "", // defult: "In process...",
-    linkm: "", // defult: "Where is the link?",
-    error: "", // defult: "Error!!",
+    notFound: "This Command not created. it was creating", // defult: "This Command not created. it was creating",
+    success: "Done ✓", // defult: "Done ✓",
+    admin: "This Feature Is Only For Admin!", // defult: "This Feature Is Only For Admin!",
+    botAdmin: "Bot Must Be Admin First!", // defult: "Bot Must Be Admin First!",
+    owner: "This Feature Is Only For Owner!", // defult: "This Feature Is Only For Owner!",
+    group: "Feature Used Only For Groups!", // defult: "Feature Used Only For Groups!",
+    private: "Features Used Only For Private Chat!", // defult: "Features Used Only For Private Chat!",
+    bot: "This Feature Is Only For Bot", // defult: "This Feature Is Only For Bot",
+    wait: "In process...", // defult: "In process...",
+    linkm: "Where is the link?", // defult: "Where is the link?",
+    error: "Error!!", // defult: "Error!!",
     endLimit: "", // defult: "Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours",
-    ban: "", // defult: "You have been banned by the owner, if you want to be unbanned, chat owner.",
-    nsfw: "", // defult: "The nsfw feature has not been activated, please contact the admin to activate",
-    banChat: "", // defult: "The bot was banned in this group, please contact the owner to unban",
+    ban: "You have been banned by the owner, if you want to be unbanned, chat owner", // defult: "You have been banned by the owner, if you want to be unbanned, chat owner.",
+    nsfw: "The nsfw feature has not been activated, please contact the admin to activate", // defult: "The nsfw feature has not been activated, please contact the admin to activate",
+    banChat: "The bot was banned in this group, please contact the owner to unban", // defult: "The bot was banned in this group, please contact the owner to unban",
   },
   setting: {
     blockchat: [], // Your block chat Jids
   },
   auto: {
     chat: {
-      group: false, // Chat Bot In Group | u can set true or false
+      group: true, // Chat Bot In Group | u can set true or false
       inbox: false, // chat bot in inbox | u can set true or false
     },
     reply: {
